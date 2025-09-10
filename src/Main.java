cae522974b63f9c07cbf8c0af42428fa0cb758e5
Kseniya Kobchenko <KKobchenko@inno.tech>
2025-09-10 21:26:50 +0300

    Сборка приложения в JAR. Задание по компиляции и запуску.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main { public static void main(String[] args) {
-    System.out.println("Случайное число от 0 до 1: " +
-            Math.random());
+public class
+
+Main { public static void main(String[] args) {
+    // Выводим приглашение для пользователя
+    System.out.print("Введите текст и нажмите <Enter>: ");
+
+// Создаем Scanner для чтения ввода из консоли
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+// Читаем всю строку, которую ввел пользователь
+    String text = scanner.nextLine();
+
+// Выводим длину введенного текста
+    System.out.println("Длина текста: " + text.length());
+
+// Закрываем scanner (хороший тон)
+    scanner.close();
 }
 }
